90647806a30c3977ffe39c53b08c51ec27135444
Sonupatel A L <19358sonupatel@gmail.com>
2025-02-10 12:14:41 +0530

    Updated the file for validating the Parenthesis

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the valid arithmetic expression: ");
+        String expression = input.nextLine();
+        System.out.println(expression);
+
+        boolean isValid = true;
+
+        isValid = isValidParenthisis(expression);
+
+        System.out.println(isValid);
+
+        //Validating the braces;
+
+
+
+
+    }
+
+    public static boolean isValidParenthisis(String expression) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : expression.toCharArray()) {
+            if(ch == '(') {
+                stack.push('(');
+            }
+            else if(ch == ')') {
+                if(!stack.empty() && stack.peek()=='('){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(ch=='['||ch=='{'||ch==']'||ch=='}') {
+                    return false;
+                }
+            }
+        }
+        if(stack.empty()){
+            return true;
+        }
+        return false;
+    }
+}
